429c19c0a99cd7d511dae9b75a743b0fe7aab22c
viveksharma-oodles <viveksharma6760@gmail.com>
2025-02-16 14:40:23 +0530

    update metthods

--- tests/pages/adminPage.ts
+++ tests/pages/adminPage.ts
@@ -13,11 +13,18 @@ export class AdminPage {
   async addUser(username: string, password: string, employeeName: string) {
     await this.page.click('button:has-text("Add")');
     await this.page.waitForSelector('[placeholder="Type for hints..."]', { state: 'visible' });
-await this.page.fill('[placeholder="Type for hints..."]', employeeName);
-await this.page.waitForSelector('[name="username"]', { state: 'visible' });
-await this.page.fill('[name="username"]', username);
-    await this.page.fill('input[name="password"]', password);
-    await this.page.fill('input[name="confirmPassword"]', password);
+    await this.page.fill('[placeholder="Type for hints..."]', employeeName);
+    await this.page.waitForSelector('//label[contains(text(), "Username")]/following-sibling::input', { state: 'visible' });
+    await this.page.fill('//label[contains(text(), "Username")]/following-sibling::input', username);
+
+    await this.page.waitForSelector('//label[contains(text(), "password")]/following-sibling::input', { state: 'visible' });
+await this.page.fill('//label[contains(text(), "password")]/following-sibling::input', password);
+
+await this.page.waitForSelector('//label[contains(text(), "confirmPassword")]/following-sibling::input', { state: 'visible' });
+await this.page.fill('//label[contains(text(), "confirmPassword")]/following-sibling::input', password);
+
+    // await this.page.fill('input[name="password"]', password);
+    // await this.page.fill('input[name="confirmPassword"]', password);
     await this.page.click('button:has-text("Save")');
   }
 }
